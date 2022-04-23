02f12c8fa1ba92fcb3d10025e694379499329020
PlatinumKellerman <platinumm@ukr.net>
2022-04-24 01:00:09 +0300

    update

--- js.js
+++ js.js
@@ -3,21 +3,24 @@ const refs = {
     inputButton: document.querySelector('#inputButton'),
     removeButton: document.querySelector('#allClearButton'),
     list: document.querySelector('.task-list'),
-    listItems: document.querySelector('.list-item')
+    listItems: document.querySelector('.list-item'),
+    form: document.querySelector('#form')
 }
 
-refs.input.addEventListener('input', onInputText)
+refs.form.addEventListener('submit', onInputTextSubmit)
 refs.inputButton.addEventListener('click', onButtonClick)
 refs.removeButton.addEventListener('click', onRemoveButtonClick)
 refs.list.addEventListener('click', onListItemClick)
+
 
 
 function onListItemClick(event) {
     event.target.classList.toggle("list-item__added");
 }
 
-function onButtonClick() {
-    if (refs.input.value) {
+function onButtonClick(e) {
+    e.preventDefault();
+    if (refs.input.value || refs.listItems) {
     const liItem = document.createElement('li');
         liItem.textContent = refs.input.value;
         refs.list.append(liItem);
@@ -30,8 +33,13 @@ function onRemoveButtonClick() {
     refs.list.innerHTML = "";
 }
 
-function onInputText() {
-    
+function onInputTextSubmit(event) {
+    event.preventDefault();
+    if (refs.input.value) {
+    const liItem = document.createElement('li');
+        liItem.textContent = refs.input.value;
+        refs.list.append(liItem);
+        liItem.classList.add('list-item');
+        refs.input.value = "";
+    }
 }
-
-
