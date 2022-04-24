fd9b35302fcc7c0fcb3bc63d9b6a48cf053541ea
PlatinumKellerman <platinumm@ukr.net>
2022-04-24 14:29:25 +0300

    Update js.js

--- js.js
+++ js.js
@@ -6,6 +6,8 @@ const refs = {
     listItems: document.querySelector('.list-item'),
     form: document.querySelector('#form')
 }
+let keyControlValue;
+
 
 refs.form.addEventListener('submit', onInputTextSubmit)
 refs.inputButton.addEventListener('click', onButtonClick)
@@ -13,14 +15,9 @@ refs.removeButton.addEventListener('click', onRemoveButtonClick)
 refs.list.addEventListener('click', onListItemClick)
 
 
-
-function onListItemClick(event) {
-    event.target.classList.toggle("list-item__added");
-}
-
 function onButtonClick(e) {
     e.preventDefault();
-    if (refs.input.value || refs.listItems) {
+    if (refs.input.value) {
     const liItem = document.createElement('li');
         liItem.textContent = refs.input.value;
         refs.list.append(liItem);
@@ -28,7 +25,13 @@ function onButtonClick(e) {
         refs.input.value = "";
     }
 }
-    
+
+function onListItemClick(evt) {
+    if (evt.target.classList.contains('list-item')) {
+    evt.target.classList.toggle('list-item__added'); 
+    }
+}
+
 function onRemoveButtonClick() {
     refs.list.innerHTML = "";
 }
@@ -41,5 +44,27 @@ function onInputTextSubmit(event) {
         refs.list.append(liItem);
         liItem.classList.add('list-item');
         refs.input.value = "";
+    }
+}
+
+addEventListener('keydown', onControlKeyPress);
+function onControlKeyPress(e) {
+    if (e.ctrlKey) {
+        e.repeat = false;
+        keyControlValue = e.key;
+    }
+}
+
+refs.list.addEventListener('click', onListItemRemoveClick)
+function onListItemRemoveClick(e) {
+    if (keyControlValue = e.key & e.target.classList.contains('list-item')) {
+        refs.listItems.innerHTML = "";
+    }   
+}
+
+addEventListener('keyup', onControlKeyUnpress)
+function onControlKeyUnpress(e) {
+    if (keyControlValue = e.key) {
+        let keyControlValue;
     }
 }
